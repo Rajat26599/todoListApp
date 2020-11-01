859e192980121d761b83bea791d84212440f53ba
Rajat Saxena <saxena1975sanjeev@gmail.com>
2020-11-01 10:47:47 +0530

    added firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAamuB5AXs9ShX0_cxj1Xtt5bwvCxsQKpU",
+    authDomain: "todolistapp-72358.firebaseapp.com",
+    databaseURL: "https://todolistapp-72358.firebaseio.com",
+    projectId: "todolistapp-72358",
+    storageBucket: "todolistapp-72358.appspot.com",
+    messagingSenderId: "285646032815",
+    appId: "1:285646032815:web:9b9aca33e0f2fb4f2da95a",
+    measurementId: "G-GSE4T7Q1CV"
+  }
 };
